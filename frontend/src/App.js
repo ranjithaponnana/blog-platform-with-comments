7d866145e715c16f98ae50cdd7fe9446d74b2e6d
ranjithaponnana <ranjithaponnana2007@gmail.com>
2026-06-22 14:22:29 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import Register from "./Register";
+import Login from "./Login";
+import Posts from "./Posts";
+import CreatePost from "./CreatePost";
+
+
+function App(){
+
+return(
+
+<div>
+
+<h1>
+Ranjitha Blog Platform
+</h1>
+
+
+<Register/>
+
+<hr/>
+
+
+<Login/>
+
+<hr/>
+
+
+<Posts/>
+
+<hr/>
+
+
+<CreatePost/>
+
+
+</div>
+
+)
+
+}
+
+
+export default App;
